c93a31df7e6878ab38be80c4ea9842312e1ef552
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-03-13 13:43:27 -0700

    build(root): remove tests exclusion from typescript config
    this was resulting in errors from the typescript eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     parserOptions: {
         tsconfigRootDir: __dirname,
-        project: ['./tsconfig.json'],
+        project: ['./tsconfig.json', './packages/**/tsconfig.json'],
     },
     plugins: [
         '@typescript-eslint',
