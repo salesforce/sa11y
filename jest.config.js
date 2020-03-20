c4f6eba16f06f121f0e568f67215d2365b4f2ba4
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-03-20 15:38:45 -0700

    build(root): add code coverage opt, threshold, ci config

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,12 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    coverageThreshold: {
+        global: {
+            branches: 80,
+            functions: 80,
+            lines: 80,
+            statements: 80,
+        },
+    },
 };
