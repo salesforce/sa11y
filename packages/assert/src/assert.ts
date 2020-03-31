635bb3bff1349b4ccc35f566dbff0deb7639f304
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-03-31 14:31:01 -0700

    improvement(add result formatter func scaffolding): assert

--- packages/assert/src/assert.ts
+++ packages/assert/src/assert.ts
@@ -5,7 +5,7 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import axe, { ElementContext, RunOptions } from 'axe-core';
+import axe, { ElementContext, Result, RunOptions } from 'axe-core';
 // TODO (Fix): Figure out how to remove 'dist' from import path
 import { extended } from '@sa11y/preset-rules/dist/extended';
 import { AxeConfig } from '@sa11y/preset-rules/dist/axeConfig';
@@ -14,6 +14,10 @@ import { AxeConfig } from '@sa11y/preset-rules/dist/axeConfig';
 // TODO (refactor): Should this be exported? Can private variables be imported in tests?
 //  Search for es6 module import version of https://www.npmjs.com/package/rewire
 export const axeRuntimeExceptionMsgPrefix = 'Error running accessibility checks using axe:';
+
+function a11yResultsFormatter(violations: Result[]): string {
+    return violations.join('\n\n');
+}
 
 /**
  * Checks DOM for accessibility issues and throws an error if violations are found.
@@ -30,6 +34,6 @@ export async function assertAccessible(dom: Document = document, rules: AxeConfi
         throw new Error(`${axeRuntimeExceptionMsgPrefix} ${e}`);
     }
     if (violations.length > 0) {
-        throw new Error(violations.join('\n\n'));
+        throw new Error(a11yResultsFormatter(violations));
     }
 }
