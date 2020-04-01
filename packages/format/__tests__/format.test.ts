3e36d2926081ff51fe2cdcc0dc3294e35bef6991
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-03-31 17:29:29 -0700

    test(format): add basic snapshot test for results formatter

--- packages/format/__tests__/format.test.ts
+++ packages/format/__tests__/format.test.ts
@@ -5,6 +5,24 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import 'global-jsdom/lib/register'; // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#required-globals
+import * as axe from 'axe-core';
+import { a11yResultsFormatter } from '..';
+
+afterEach(() => {
+    document.body.innerHTML = ''; // reset dom body
+});
+
 describe('format', () => {
-    it.todo('move result snapshot matching test from assert test');
+    it('dom with a11y issues', async () => {
+        document.body.innerHTML = `<html>
+                                    <body>
+                                     <a href="#"></a>
+                                    </body>
+                                   </html>`;
+        await axe.run(document).then((results) => {
+            expect(results).toBeDefined();
+            expect(a11yResultsFormatter(results.violations)).toMatchSnapshot();
+        });
+    });
 });
