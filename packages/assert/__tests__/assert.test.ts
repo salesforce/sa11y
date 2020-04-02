7b9e2ef78d3f8a6135758394cd0218f320ee65ea
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-04-02 15:47:47 -0700

    test(assert): add explicit assertion expectations for async tests
    rename test names to be more descriptive

--- packages/assert/__tests__/assert.test.ts
+++ packages/assert/__tests__/assert.test.ts
@@ -7,6 +7,7 @@
 
 import 'global-jsdom/lib/register'; // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#required-globals
 import { assertAccessible, axeRuntimeExceptionMsgPrefix } from '../src/assert';
+// TODO (Fix): Remove "dist" from import path
 import { extended } from '@sa11y/preset-rules/dist/extended';
 import { getA11yConfig } from '@sa11y/preset-rules/dist/a11yConfig';
 
@@ -24,17 +25,17 @@ afterEach(() => {
     document.body.innerHTML = ''; // reset dom body
 });
 
-describe('assert', () => {
-    it('non existent rule triggers axe runtime exception', async () => {
-        // TODO: Rename getA11yConfig() to getA11yConfig
+describe('assertAccessible API', () => {
+    it('triggers axe runtime exception for non existent rule', async () => {
         const errConfig = getA11yConfig(['non-existent-rule']);
+        expect.assertions(2);
         await assertAccessible(document, errConfig).catch((e) => {
             expect(e).toBeDefined();
             expect(e.toString()).toContain(axeRuntimeExceptionMsgPrefix);
         });
     });
 
-    it('dom with no a11y issues', async () => {
+    it('throws no errors for dom with no a11y issues', async () => {
         // TODO (cleanup): Move inline html content into test data files
         // From https://github.com/dequelabs/axe-selenium-java/blob/develop/src/test/resources/test-app.js-->
         document.body.innerHTML = `<!doctype html>
@@ -54,15 +55,17 @@ describe('assert', () => {
                                         </script>
                                     </body>
                                     </html>`;
-        await assertAccessible(document, jsdomRules).catch((error) => expect(error).toBeUndefined());
+        expect.assertions(0); // Need at least one expect in a test
+        await assertAccessible(document, jsdomRules); // No error thrown
     });
 
-    it('dom with a11y issues', async () => {
+    it('throws an error with a11y issues found for dom with a11y issues', async () => {
         document.body.innerHTML = `<html>
                                     <body>
                                      <a href="#"></a>
                                     </body>
                                    </html>`;
+        expect.assertions(3);
         await assertAccessible(document, jsdomRules).catch((e) => {
             expect(e).toBeDefined();
             expect(e.toString()).not.toContain(axeRuntimeExceptionMsgPrefix);
