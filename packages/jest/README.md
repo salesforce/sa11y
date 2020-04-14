51ec35dd1867bf2467cf1fe5f3ffbb8adc880e7c
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-04-13 20:58:05 -0700

    feat(jest): add scaffolding for jest expect matcher

--- packages/jest/README.md
+++ packages/jest/README.md
@@ -1,9 +1,15 @@
-# `format`
+# `jest`
 
-Format accessibility results from axe
+Accessibility helpers for [Jest](https://jestjs.io)
 
 ## Usage
 
 ```
-TODO
+import { toBeAccessible } from '@sa11y/jest'
+
+// Setup DOM to be tested for accessibility
+// ...
+
+// Assert that the DOM is accessible
+expect().toBeAccessible()
 ```
--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -4,3 +4,15 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+
+import { assertAccessible } from '@sa11y/assert';
+
+async function toBeAccessible(dom: Document = document) {
+    await assertAccessible(dom);
+    return {
+        pass: true,
+        message: () => '',
+    };
+}
+
+expect.extend(toBeAccessible);
