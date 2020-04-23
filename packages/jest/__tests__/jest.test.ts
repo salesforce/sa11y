e8055fa86815c88d78ae53e3dc4bd7ba9bfa9796
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-04-23 13:33:00 -0700

    improvement(jest): flesh out custom jest axe matcher
    add type def, tests

--- packages/jest/__tests__/jest.test.ts
+++ packages/jest/__tests__/jest.test.ts
@@ -6,11 +6,55 @@
  */
 
 import 'global-jsdom/lib/register'; // https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#required-globals
+import { toBeAccessible } from '../src/jest';
+import { extended, recommended } from '@sa11y/preset-rules';
+
+// TODO (de-duplicate): Extract into a common place and reuse across packages
+const domWithA11yIssues = `<html>
+                            <body>
+                             <a href="#"></a>
+                            </body>
+                           </html>`;
+
+// From https://github.com/dequelabs/axe-selenium-java/blob/develop/src/test/resources/test-app.js-->
+const domWithNoA11yIssues = `<!doctype html>
+                            <html lang="en">
+                            <head>
+                                <title>Test Page</title>
+                            </head>
+                            <body>
+                            <div role="main" id="host">
+                                <h1>This is a test</h1>
+                                <p>This is a test page with no violations</p>
+                            </div>
+                            <div role="contentinfo" id="upside-down"></div> <!-- cSpell:disable-line -->
+                                <script>
+                                    var shadow = document.getElementById("upside-down").attachShadow({mode: "open"});
+                                    shadow.innerHTML = '<h2 id="shadow">SHADOW DOM</h2><ul><li>Shadow Item 1</li></ul>'
+                                </script>
+                            </body>
+                            </html>`;
+
+beforeAll(() => {
+    expect.extend({ toBeAccessible });
+});
 
 afterEach(() => {
     document.body.innerHTML = ''; // reset dom body
 });
 
-describe('format', () => {
-    it.todo('dom with a11y issues');
+describe('toBeAccessible Jest API', function () {
+    // TODO (refactor): Sep basic functionality into their own tests from testing default args
+    //  Use https://github.com/dankogai/js-combinatorics
+    document.body.innerHTML = domWithA11yIssues;
+    it.each([document, undefined])('throws no error for dom with no a11y issues: %#', (dom) => {
+        expect(dom).toBeAccessible();
+    });
+
+    it.each([extended, recommended, undefined])(
+        'toBeAccessible throws error for dom with a11y issues with config: %#',
+        (config) => {
+            expect(domWithA11yIssues).toBeAccessible(config);
+        }
+    );
 });
--- packages/jest/src/jest.ts
+++ packages/jest/src/jest.ts
@@ -6,13 +6,37 @@
  */
 
 import { assertAccessible } from '@sa11y/assert';
+import { A11yConfig, extended } from '@sa11y/preset-rules';
 
-async function toBeAccessible(dom: Document = document) {
-    await assertAccessible(dom);
+// Type def for custom jest a11y matcher toBeAccessible
+// Ref: https://jestjs.io/docs/en/expect.html#expectextendmatchers
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace jest {
+        interface Matchers<R> {
+            toBeAccessible(config?: A11yConfig): R;
+        }
+    }
+}
+
+/**
+ * Jest expect matcher to check document for accessibility issues
+ * @param received - Document to be tested for accessibility
+ * @param config - A11yConfig to be used to test for accessibility. Defaults to extended.
+ */
+export async function toBeAccessible(received: Document = document, config: A11yConfig = extended) {
+    let isAccessible = true;
+    let a11yViolations = '';
+    try {
+        await assertAccessible(received, config);
+    } catch (e) {
+        isAccessible = false;
+        a11yViolations = e;
+    }
     return {
-        pass: true,
-        message: () => '',
+        pass: isAccessible,
+        message: (): string => `expected document to have no accessibility violations ${a11yViolations}`,
     };
 }
 
-expect.extend(toBeAccessible);
+expect.extend({ toBeAccessible });
