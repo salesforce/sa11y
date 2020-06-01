a6f41b335b13529585687f372a05a20780754d4e
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-06-01 10:15:55 -0700

    Revert "revert(format): remove print received jest helper"
    This reverts commit 72c5e282adff2dba822dac4d60f657d9f6f06667.
    
    # Conflicts:
    #	packages/jest/__tests__/__snapshots__/matcher.test.ts.snap

--- packages/format/src/format.ts
+++ packages/format/src/format.ts
@@ -6,7 +6,7 @@
  */
 
 import { Result } from 'axe-core';
-// import { printReceived } from 'jest-matcher-utils';
+import { printReceived } from 'jest-matcher-utils';
 
 const a11yViolationIndicator = '⭕';
 const helpUrlIndicator = '🔗';
@@ -43,7 +43,7 @@ export function a11yResultsFormatter(violations: Result[]): string {
                     const helpURL = violation.helpUrl.split('?')[0];
                     return (
                         // TODO: Create a formatter specifically for Jest using printReceived etc?
-                        `${a11yViolationIndicator} (${violation.id}) ${violation.help}: ${selectors}` +
+                        printReceived(`${a11yViolationIndicator} (${violation.id}) ${violation.help}: ${selectors}`) +
                         `\n\t${helpUrlIndicator} Help URL: ${helpURL}`
                     );
                 })
