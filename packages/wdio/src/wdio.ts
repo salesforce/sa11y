0aa8eb0f675b3822956947579261b4fc4b6a8f8d
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-06-17 13:43:12 -0700

    refactor(wdio): remove redundant run script, reorg imports

--- packages/wdio/src/wdio.ts
+++ packages/wdio/src/wdio.ts
@@ -4,9 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-import { A11yConfig, extended } from '@sa11y/preset-rules';
+
 import * as axe from 'axe-core';
+import { A11yConfig, extended } from '@sa11y/preset-rules';
 import { axeRuntimeExceptionMsgPrefix } from '@sa11y/common';
 import { A11yError } from '@sa11y/format';
 import { BrowserObject } from 'webdriverio';
@@ -18,6 +18,7 @@ export async function assertAccessible(driver: BrowserObject = browser, rules: A
     // TODO (refactor): Re-eval dependencies and check each one if it is necessary
     //  - Move from dev dep to dep or peer dep
     // TODO (deduplicate): with @sa11y/assert
+    // TODO (feat): adapt a11y config based on if browser is running in headless or not
     let violations;
     try {
         // inject the script
