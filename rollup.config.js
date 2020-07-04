616f2442308f85225ac395d985146fcdd4ec7347
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-07-03 21:07:32 -0700

    build(rollup): fix and override module format in rollup config
    revert module in tsconfig common as it was breaking wdio tests

--- rollup.config.js
+++ rollup.config.js
@@ -11,10 +11,15 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 import sizes from 'rollup-plugin-sizes';
 import { terser } from 'rollup-plugin-terser';
+// TODO (refactor): reuse entry-points from package json
+// import pkg from './package.json';
 
+// TODO (debug): Warning (!) `this` has been rewritten to `undefined` in axe.js
 export default {
+    // TODO (refactor): Move this rollup config file to package/assert ?
     input: 'packages/assert/src/assert.ts',
     output: {
+        // TODO (refactor): add "browser" entry point in package.json and use "pkg.browser"
         file: 'sa11y.min.js',
         format: 'iife', // Should this be 'iife' for browser ?
         name: 'sa11y',
@@ -23,7 +28,12 @@ export default {
         progress({ clearLine: false }),
         resolve(),
         commonjs(), // TODO (debug): Why do we need CJS transformation even when tsconfig has "module": "es2015" ?
-        typescript({ tsconfig: 'packages/assert/tsconfig.json', useTsconfigDeclarationDir: true }),
+        // TODO (Fix): override to use different module in rollup config than tsconfig
+        typescript({
+            tsconfig: 'packages/assert/tsconfig.json',
+            tsconfigOverride: { compilerOptions: { module: 'es2015' } },
+            useTsconfigDeclarationDir: true,
+        }),
         sizes({ details: true }),
         terser(),
     ],
