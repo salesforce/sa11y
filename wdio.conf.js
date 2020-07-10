9de67768c0b00f1216bff46ca87d28c1b6874d1a
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-07-09 18:57:17 -0700

    test(wdio): add log output dir when in debug mode

--- wdio.conf.js
+++ wdio.conf.js
@@ -95,6 +95,7 @@ exports.config = {
     //     '@wdio/applitools-service': 'info'
     // },
     //
+    outputDir: debug ? '/tmp/wdio/' : '',
     // If you only want to run your tests until a specific amount of tests have failed use
     // bail (default is 0 - don't bail, run all tests).
     bail: debug ? 1 : 0,
