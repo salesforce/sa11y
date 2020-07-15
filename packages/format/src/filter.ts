502e9897c6bd46be11ec143a5777518cde2dcc56
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-07-14 19:10:43 -0700

    feat(format): add a exception list filter to filter a11y violations

---
+++ packages/format/src/filter.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { Result } from 'axe-core';
+
+/**
+ * Filter to post-process a11y results from axe
+ */
+export interface Filter {
+    (violations: Result[]): Result[];
+}
+
+// TODO (refactor): constrain rule id to known rule ids e.g using string literal, keyof, in etc
+//  e.g. https://stackoverflow.com/a/54061487
+// const ruleIDs = getRules().map((ruleObj) => ruleObj.ruleId);
+// type ruleID = keyof ruleIDs;
+// type ruleID = typeof ruleIDs[number];
+type ruleID = string;
+type cssSelectors = string[];
+/**
+ * Exception list of map of rule to corresponding css targets that needs to be filtered from a11y results.
+ */
+type exception = Record<ruleID, cssSelectors>;
+
+export function exceptionListFilter(violations: Result[], exceptionList: exception[]): Result[] {
+    return violations.filter((violation) =>
+        Object.keys(exceptionList).includes(violation.id)
+            ? violation.nodes.filter((result) =>
+                  result.target.filter((cssSelector) => exceptionList[violation.id].includes(cssSelector))
+              )
+            : true
+    );
+}
