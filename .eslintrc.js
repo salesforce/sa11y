3c4ddd011cccccd0209ac6b4fc93a86483ffc87a
Mohan Raj Rajamanickam <mrajamanickam@salesforce.com>
2020-09-03 13:42:21 -0700

    test(eslint): enable typescript-eslint/recommended-requiring-type-checking

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,7 +30,7 @@ module.exports = {
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
         // TODO (fix): Enabling this results in 80+ errors with new versions of @typescript-eslint and typescript v4. Enable and fix the errors
-        // 'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:jest/recommended',
         'plugin:jest/style',
         'plugin:prettier/recommended',
