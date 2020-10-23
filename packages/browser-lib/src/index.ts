95e8ae6d6340572abfd4fc3ed10f1a28d4dc276d
Mohan Raj R <mohanraj.cit@gmail.com>
2020-10-22 19:24:39 -0700

    refactor(browser-lib): remove type annotations due to error with ts plugins

--- packages/browser-lib/src/index.ts
+++ packages/browser-lib/src/index.ts
@@ -6,21 +6,23 @@
  */
 
 import * as axe from 'axe-core';
-import { exceptionListFilter, exceptionList } from '@sa11y/format';
-import { A11yConfig, recommended } from '@sa11y/preset-rules';
+import { exceptionListFilter } from '@sa11y/format';
+import { recommended } from '@sa11y/preset-rules';
 
 export const namespace = 'sa11y';
 
 /**
  * Wrapper function to check accessibility to be invoked after the sa11y minified JS file
  * is injected into the browser
- * @param exceptionList - {@link exceptionList} mapping of rule to css selectors to be filtered out using {@link exceptionListFilter}
- * @param rules - preset sa11y rules as {@link A11yConfig} defaulting to {@link recommended}
+ * @param exceptionList - mapping of rule to css selectors to be filtered out using {@link exceptionListFilter}
+ * @param rules - preset sa11y rules defaulting to {@link recommended}
  */
-export async function checkAccessibility(
-    exceptionList: exceptionList = {},
-    rules: A11yConfig = recommended
-): Promise<axe.Result[]> {
+export async function checkAccessibility(exceptionList = {}, rules = recommended) {
+    // TODO (debug): adding type annotations to args, return type results in error:
+    //  "[!] Error: Unexpected token" in both rollup-plugin-typescript2 and @rollup/plugin-typescript
+    //  Compiling the index.ts file using tsc and using the dist/index.js file results
+    //  in error when importing the "namespace" var. This would probably be easier to fix
+    //  which could then result in getting rid of the rollup typescript plugins.
     const results = await axe.run(rules);
     return exceptionListFilter(results.violations, exceptionList);
 }
