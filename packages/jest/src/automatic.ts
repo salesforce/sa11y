de2ef033b3dc5865347d500b9a4060a6caae78d4
Mohan Raj R <mohanraj.cit@gmail.com>
2021-02-19 20:15:05 -0800

    refactor: switch back to using dom walker and consolidate violations from all nodes

--- packages/jest/src/automatic.ts
+++ packages/jest/src/automatic.ts
@@ -5,6 +5,10 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+import { AxeResults } from '@sa11y/common';
+import { getViolationsJSDOM } from '@sa11y/assert';
+import { A11yError } from '@sa11y/format';
+
 /**
  * Options for Automatic checks to be passed to {@link registerSa11yAutomaticChecks}
  */
@@ -30,18 +34,25 @@ const defaultAutoCheckOpts: AutoCheckOpts = {
  * @param cleanup - boolean indicating if the DOM should be cleaned up after
  */
 export async function automaticCheck(cleanup = false): Promise<void> {
-    let currNode = document.body.firstElementChild;
-    while (currNode !== null) {
-        try {
+    const violations: AxeResults = [];
+    // Create a DOM walker filtering only elements (skipping text, comment nodes etc)
+    const walker = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT);
+    let currNode = walker.firstChild();
+    try {
+        while (currNode !== null) {
             // TODO (spike): Use a logger lib with log levels selectable at runtime
-            // console.log('♿ [DEBUG] Automatically checking a11y of ' + currNode.nodeName);
-            await expect(currNode).toBeAccessible();
-        } finally {
-            if (cleanup) document.body.removeChild(currNode);
-            currNode = currNode.nextElementSibling;
+            // console.log(
+            //     `♿ [DEBUG] Automatically checking a11y of ${currNode.nodeName}
+            //      for test "${expect.getState().currentTestName}"
+            //      : ${expect.getState().testPath}`
+            // );
+            violations.push(...(await getViolationsJSDOM(currNode)));
+            currNode = walker.nextSibling();
         }
+    } finally {
+        if (cleanup) document.body.innerHTML = ''; // remove non-element nodes
+        A11yError.checkAndThrow(violations);
     }
-    if (cleanup) document.body.innerHTML = ''; // remove non-element nodes
 }
 
 /**
