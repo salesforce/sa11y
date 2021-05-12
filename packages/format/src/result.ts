77b72771c6e7f90615b0180842a4d8c3f45a4839
Mohan Raj R <mohanraj.cit@gmail.com>
2021-05-11 19:53:15 -0700

    refactor: use map to consolidate results; add a util to convert axe result

--- packages/format/src/result.ts
+++ packages/format/src/result.ts
@@ -5,27 +5,57 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 import { AxeResults } from '@sa11y/common';
+import { ImpactValue } from 'axe-core';
+import * as axe from 'axe-core';
 
 /**
- * Remap, filter info from axe result
+ * Filtered a11y result containing essential info about the a11y failure
  */
-// class A11yResult {
-//     constructor(AxeResult) {}
-// }
-//
-// type RuleIdCssSelectorMap = Map<RuleID, CssSelectors>;
+export type A11yResult = {
+    id: string;
+    selectors: string;
+    description: string;
+    helpUrl: string;
+    impact: ImpactValue;
+    // TODO: Add WCAG levels to sort rule
+    wcagVersion: number; // TODO: convert to enum
+    wcagLevel: string; // TODO: convert to enum
+    wcagSC: string;
+    testPath: string;
+    testNames: string[];
+};
+
+export type A11yResults = A11yResult[];
 
 /**
  * Consolidate unique a11y violations by removing duplicates.
  */
 export class ConsolidatedResults {
-    // TODO (refactor): Is there any advantage to extending built-in Set/Map ?
-    // TODO (refactor): Would it be more efficient to recast into a Map struct?
-    // static consolidated = new Map<RuleIdCssSelectorMap, AxeResult>();
-    static consolidated: AxeResults = [];
+    // static consolidatedMap = new Map<string, A11yResult>();
+    static consolidatedMap = new Map<string, axe.Result>();
 
     static clear(): void {
-        this.consolidated = [];
+        this.consolidatedMap.clear();
+    }
+
+    static convert(results: AxeResults): A11yResults {
+        const a11yResults: A11yResults = [];
+        for (const result of results) {
+            for (const node of result.nodes) {
+                a11yResults.push(<A11yResult>{
+                    // TODO (refactor): Reuse A11yResult for A11yError.format()
+                    id: result.id,
+                    // Note: Use a separator that cannot be part of a CSS selector
+                    selectors: node.target.sort().join('; '),
+                    description: result.help,
+                    helpUrl: result.helpUrl.split('?')[0],
+                    impact: result.impact,
+                    // TODO : Add wcag level or best practice tag to output ?
+                    // const criteria = violation.tags.filter((tag) => tag.startsWith('wcag2a') || tag.startsWith('best'));
+                });
+            }
+        }
+        return a11yResults;
     }
 
     /**
@@ -33,26 +63,19 @@ export class ConsolidatedResults {
      * @returns results that have not been added earlier
      */
     static add(results: AxeResults): AxeResults {
-        // TODO (perf): convert consolidated results to a map struct with faster lookup
-        //  instead of multi-level looping over each result?
         const newResults: AxeResults = [];
         for (const result of results) {
-            let unique = true;
-            for (const consolidatedResult of this.consolidated) {
-                if (result.id !== consolidatedResult.id) continue;
-                for (const node of result.nodes) {
-                    for (const consolidatedNode of consolidatedResult.nodes) {
-                        if (
-                            node.target.length === consolidatedNode.target.length &&
-                            node.target.filter((selector) => !consolidatedNode.target.includes(selector)).length === 0
-                        )
-                            unique = false;
-                    }
+            for (const node of result.nodes) {
+                const selectors = node.target.sort().join(';');
+                const key = `${result.id}--${selectors}`;
+                if (!this.consolidatedMap.has(key)) {
+                    this.consolidatedMap.set(key, result);
+                    // TODO (refactor): Store A11yResult with selected fields instead
+                    //  AND reuse it to display formatted error message
+                    newResults.push(result);
                 }
             }
-            if (unique) newResults.push(result);
         }
-        this.consolidated.push(...newResults);
         return newResults;
     }
 }
