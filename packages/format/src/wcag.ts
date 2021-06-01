dfd4cfb976949854fb5d8f60007829595a908d5e
Mohan Raj R <mohanraj.cit@gmail.com>
2021-06-01 14:15:38 -0700

    refactor: remove sc prefix for best practice

--- packages/format/src/wcag.ts
+++ packages/format/src/wcag.ts
@@ -14,29 +14,31 @@ type WcagVersion = '2.0' | '2.1' | undefined;
  */
 export class WcagMetadata {
     static readonly regExp = /^(wcag)(?<version_or_sc>\d+)(?<level>a*)$/;
+    // Default SC for axe rules not strictly associated with a WCAG SC
     static readonly defaultSC = 'best-practice';
     public wcagLevel: WcagLevel;
     public wcagVersion: WcagVersion;
-    public successCriteria = WcagMetadata.defaultSC; // Default SC for non-wcag rules
+    public successCriteria = WcagMetadata.defaultSC;
+
     constructor(readonly tags: string[]) {
-        tags.forEach((tag) => {
+        for (const tag of tags) {
             const match = WcagMetadata.regExp.exec(tag);
-            if (match && match.groups) {
-                const level = match.groups.level;
-                // Tags starting with "wcag" can contain either wcag version and level
-                // or success criteria
-                if (level) {
-                    this.wcagLevel = level.toUpperCase() as WcagLevel;
-                    if (match.groups.version_or_sc === '2') {
-                        this.wcagVersion = '2.0'; // Add decimal for consistency
-                    } else {
-                        this.wcagVersion = match.groups.version_or_sc.split('').join('.') as WcagVersion;
-                    }
+            if (!match || !match.groups) continue;
+            const level = match.groups.level;
+            const versionOrSC = match.groups.version_or_sc.split('').join('.');
+            // Tags starting with "wcag" can contain either wcag version and level
+            // or success criteria e.g. "wcag2aa", "wcag111"
+            if (level) {
+                this.wcagLevel = level.toUpperCase() as WcagLevel;
+                if (versionOrSC === '2') {
+                    this.wcagVersion = '2.0'; // Add decimal for consistency
                 } else {
-                    this.successCriteria = match.groups.version_or_sc.split('').join('.');
+                    this.wcagVersion = versionOrSC as WcagVersion;
                 }
+            } else {
+                this.successCriteria = `${versionOrSC}`;
             }
-        });
+        }
     }
 
     /**
@@ -46,6 +48,8 @@ export class WcagMetadata {
         if (!this.wcagVersion || !this.wcagLevel) {
             throw new Error(`Unable to set WCAG version and level from given tags: ${this.tags.join(', ')}`);
         }
-        return `WCAG-${this.wcagVersion}-Level-${this.wcagLevel} SC-${this.successCriteria}`;
+        const sc =
+            this.successCriteria === WcagMetadata.defaultSC ? WcagMetadata.defaultSC : `SC-${this.successCriteria}`;
+        return `WCAG-${this.wcagVersion}-Level-${this.wcagLevel} ${sc}`;
     }
 }
