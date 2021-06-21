88c30a6aa6a1d8dc1f08111c02b161e5a8226f33
Mohan Raj R <mohanraj.cit@gmail.com>
2021-06-21 12:12:55 -0700

    refactor: add a debug flag to enable console log msg and silence by default

--- packages/common/src/helpers.ts
+++ packages/common/src/helpers.ts
@@ -7,7 +7,10 @@
 
 /**
  * Convenience wrapper to prefix a standard header for console log messages.
+ * Logging is enabled only when environment variable `SA11Y_DEBUG` is set.
  */
 export function log(...args: unknown[]): void {
-    console.log('♿[Sa11y]', ...args);
+    // Probably not worth it to mock and test console logging for this helper util
+    /* istanbul ignore next */
+    if (process.env.SA11Y_DEBUG) console.log('♿[Sa11y]', ...args);
 }
