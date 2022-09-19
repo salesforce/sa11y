cd892bc0034850d4f965d87aecce5aed673dc6e5
Jason Schroeder <jschroeder@salesforce.com>
2022-09-19 11:21:15 -0700

    docs: add type hints to babel.config.js
    Helps with editor autocomplete

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+/** @type {import('@babel/core').ConfigFunction} */
 module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }], '@babel/preset-typescript'],
 };
