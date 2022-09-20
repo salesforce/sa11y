56982bd7d4924526fa1919f27673e832dd1dd254
Jason Schroeder <jschroeder@salesforce.com>
2022-09-20 14:33:26 -0700

    refactor(common): remove custom typedef WdioBrowser
    In Webdriverio 7, there are differences between `WebdriverIO.Browser` and
    `WebdriverIO.MultiRemoteBrowser` in the functional types and arguments.
    
    For simplicity, we'll only support single browser sessions.

--- packages/common/src/wdio.ts
+++ packages/common/src/wdio.ts
@@ -4,27 +4,21 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { BrowserObject, MultiRemoteBrowserObject, Element } from 'webdriverio';
 import { A11yConfig } from './axe';
 import { ExceptionList } from './format';
 
-// TODO (refactor): The base common pkg depending on wdio feels strange.
-//  Refactor it out into another package, even a new one (e.g. test-utils-wdio) to
-//  avoid circular dep.
-export type WdioBrowser = BrowserObject | MultiRemoteBrowserObject;
-
 /**
  * Optional arguments passed to WDIO APIs
- * @param driver - WDIO {@link BrowserObject} instance navigated to the page to be checked. Created automatically by WDIO test runner. Might need to be passed in explicitly when other test runners are used.
+ * @param driver - WDIO {@link WebdriverIO.Browser} instance navigated to the page to be checked. Created automatically by WDIO test runner. Might need to be passed in explicitly when other test runners are used.
  * @param scope - Element to check for accessibility found using [`browser.$(selector)`](https://webdriver.io/docs/selectors), defaults to the entire document.
  * @param rules - {@link A11yConfig} to be used for checking accessibility. Defaults to {@link base}
  * @param exceptionList - map of rule id to corresponding CSS targets that needs to be filtered from results
  */
-export interface WdioOptions {
-    driver: WdioBrowser;
-    scope?: Promise<Element> | Element;
+export type WdioOptions = {
+    driver: WebdriverIO.Browser;
+    scope?: Promise<WebdriverIO.Element> | WebdriverIO.Element;
     rules?: A11yConfig;
     exceptionList?: ExceptionList;
-}
+};
 
 export type WdioAssertFunction = (opts: Partial<WdioOptions>) => void | Promise<void>;
