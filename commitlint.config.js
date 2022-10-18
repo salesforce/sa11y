ff9c52b133c548070861f3d6a5ef9c579db704b8
Jason Schroeder <jschroeder@salesforce.com>
2022-10-18 09:04:20 -0700

    build: conventional commit should allow semantic-release commit messages
    Allow conventional commit message format.

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,11 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
+/* This is the pattern from semantic-release */
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci\]/;
+
 /** @type {import('@commitlint/types').UserConfig} */
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    ignores: [(commitMsg) => automaticCommitPattern.test(commitMsg)],
 };
