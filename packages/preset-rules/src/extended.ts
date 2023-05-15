22c54fcf41e2da278bbc717cff696d2baf632c4c
Navateja Alagam <nalagam@salesforce.com>
2023-05-15 16:39:18 +0530

    chore: axe core 4.7.0 upgrade

--- packages/preset-rules/src/extended.ts
+++ packages/preset-rules/src/extended.ts
@@ -12,6 +12,7 @@ export const extendedRulesInfo: RuleInfo = new Map([
     ...baseRulesInfo,
     ...(new Map(
         Object.entries({
+            'color-contrast-enhanced': { priority: 'P3', wcagSC: '1.4.6', wcagLevel: 'AAA' },
             'css-orientation-lock': { priority: 'P3', wcagSC: '1.3.4', wcagLevel: 'AA' },
             'focus-order-semantics': { priority: 'P3', wcagSC: '', wcagLevel: '' },
             'frame-tested': { priority: 'P3', wcagSC: '', wcagLevel: '' },
@@ -20,12 +21,14 @@ export const extendedRulesInfo: RuleInfo = new Map([
             'identical-links-same-purpose': { priority: 'P3', wcagSC: '2.4.9', wcagLevel: 'AAA' },
             'label-content-name-mismatch': { priority: 'P2', wcagSC: '2.5.3', wcagLevel: 'A' },
             'link-in-text-block': { priority: 'P1', wcagSC: '1.4.1', wcagLevel: 'A' },
+            'meta-refresh-no-exceptions': { priority: 'P3', wcagSC: '', wcagLevel: '' },
             'no-autoplay-audio': { priority: 'P1', wcagSC: '1.4.2', wcagLevel: 'A' },
             'p-as-heading': { priority: 'P2', wcagSC: '1.3.1', wcagLevel: 'A' },
             'region': { priority: 'P3', wcagSC: '', wcagLevel: '' },
             'skip-link': { priority: 'P3', wcagSC: '', wcagLevel: '' },
             'table-duplicate-name': { priority: 'P3', wcagSC: '', wcagLevel: '' },
             'table-fake-caption': { priority: 'P3', wcagSC: '', wcagLevel: '' },
+            'target-size': { priority: 'P3', wcagSC: '2.5.5', wcagLevel: 'AAA' },
             'td-has-header': { priority: 'P1', wcagSC: '1.3.1', wcagLevel: 'A' },
         })
     ) as RuleInfo),
