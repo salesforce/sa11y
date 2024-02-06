5d4b0fb506c8bb9ddf50173112d1bacf3117d639
jaig-0911 <63843872+jaig-0911@users.noreply.github.com>
2024-02-06 11:00:36 +0530

    feat: update axe-core to v4.8.3 (#637)
    Co-authored-by: Navateja Alagam <navateja215@gmail.com>

--- packages/preset-rules/src/full.ts
+++ packages/preset-rules/src/full.ts
@@ -8,7 +8,12 @@ import { defaultPriority, getA11yConfig } from './rules';
 import { extendedRulesInfo } from './extended';
 
 // Rules that have been excluded due to being new or deprecated by axe
-export const excludedRules = [];
+export const excludedRules = [
+    'aria-braille-equivalent',
+    'aria-conditional-attr',
+    'aria-prohibited-attr',
+    'aria-deprecated-role',
+];
 
 // Add excluded rules to extended to get the full list
 excludedRules.forEach((rule) => extendedRulesInfo.set(rule, { priority: defaultPriority, wcagSC: '', wcagLevel: '' }));
