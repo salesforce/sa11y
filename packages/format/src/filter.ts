9899065450843212d2c93d318fe7254060312f5a
Navateja Alagam <nalagam@salesforce.com>
2024-02-09 08:57:00 +0530

    feat: ability to filter violations based on selctor keywords

--- packages/format/src/filter.ts
+++ packages/format/src/filter.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import * as axe from 'axe-core';
 import { AxeResults, ExceptionList } from '@sa11y/common';
 
 /**
@@ -33,4 +34,40 @@ export function exceptionListFilter(violations: AxeResults, exceptionList: Excep
     }
 
     return filteredViolations;
+}
+
+/**
+ * Filter a11y violations from axe based on given selectors filter keywords
+ * @param violations - List of violations found with axe
+ * @param selectorFilterKeywords - List of selector keywords to filter violations for
+ */
+export function exceptionListFilterSelectorKeywords(
+    violations: AxeResults,
+    selectorFilterKeywords: string[]
+): AxeResults {
+    const filteredViolations: AxeResults = [];
+    for (const violation of violations) {
+        const filteredNodes: axe.NodeResult[] = [];
+        for (const node of violation.nodes) {
+            const isSelectorFilterKeywordsExists = checkSelectorFilterKeyWordsExists(node, selectorFilterKeywords);
+            if (!isSelectorFilterKeywordsExists) {
+                filteredNodes.push(node);
+            }
+        }
+        if (filteredNodes.length > 0) {
+            violation.nodes = filteredNodes;
+            filteredViolations.push(violation);
+        }
+    }
+    return filteredViolations;
+}
+
+function checkSelectorFilterKeyWordsExists(node: axe.NodeResult, selectorFilterKeywords: string[]): boolean {
+    const selectorAncestry = node.ancestry?.flat(Infinity) ?? [];
+    let isExists = false;
+    selectorFilterKeywords.some((keyword) => {
+        isExists = selectorAncestry.some((selector) => selector.includes(keyword));
+        return isExists;
+    });
+    return isExists;
 }
